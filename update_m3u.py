73784ea9216ded3c35ad577f5f8d247125015f93
marvvven <dagdoug.marwen@hotmail.com>
2025-01-08 22:26:08 -0500

    Update update_m3u.py

--- update_m3u.py
+++ update_m3u.py
@@ -1,14 +1,15 @@
 import requests
-from bs4 import BeautifulSoup
 
-# Define the URL of the page where the M3U file is hosted
+# Define the URL of the M3U file (you want to fetch from)
 url = "https://a5.pokaz.me/sm9O6m6fgg-RmtvoG9IE2Q/229/1736407741/index.m3u8"
 
 # Send a request to fetch the content of the URL
 response = requests.get(url)
+
+# Check if the request was successful
 if response.status_code == 200:
-    # Open the M3U file for writing
-    with open('updated_playlist.m3u8', 'w') as file:
+    # Write the content of the M3U file
+    with open('updated_playlist.m3u', 'w') as file:
         file.write(response.text)
     print("M3U file updated successfully.")
 else:
